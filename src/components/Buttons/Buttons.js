8cd82ddc0959eb9726b2cb222a87324e169e9ea3
HassunMohsen <hassun9992@gmail.com>
2024-11-15 22:51:51 +0200

    Initialize project using Create React App

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -10,6 +10,7 @@ export default function Buttons() {
             setBtn(''); // Clear all
         } else if (value === 'c') {
             setBtn(btnNumber.slice(0, -1)); // Remove last character
+            
         } else {
             setBtn(btnNumber + value); // Append number or operator
         }
@@ -28,15 +29,30 @@ export default function Buttons() {
             </li>
         );
     };
+    function squares() {
+        return Array.from({ length: 8 }, (_, i) => <span key={i}></span>);
+    }
 
     return (
         <div className="body">
         <section className="buttons container">
+        <div className="title">
+            <h1>
+            <span>BLACK-BEAR</span>
+            <p>CALCULATOR</p>
+            </h1>
+            <div className="color">
+                {squares()}
+            </div>
+        </div>
+        <div className="input-container">
+            <p>{btnNumber}</p>
             <input type="text" 
                 value={btnNumber} 
                 onChange={e => setBtn(e.target.value)} 
                 // style={{ width: "100%", position: "absolute", top: "0",height:'50px' }} 
-            />
+                />
+        </div>
             
             <ul style={{ position: 'relative' }}>
                 {buttons.map(renderButton)}
@@ -44,7 +60,9 @@ export default function Buttons() {
                 <li className="equal" >
                     <button onClick={() => {
                         try {
+                            
                             setBtn(evaluate(btnNumber));
+                            console.log(btnNumber.type)
                         } catch (error) {
                             console.error("Invalid expression");
                         }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './all.css'
+import './all.min.css'
 import App from './components/App/App'
 import reportWebVitals from './reportWebVitals';
 
