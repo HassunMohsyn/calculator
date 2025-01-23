dd20dfe1b63004ea0105f817add1d44ce521d306
HassunMohsyn <hassun9992@gmail.com>
2025-01-23 13:55:48 +0200

    first commit

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -4,13 +4,15 @@ import { evaluate } from 'mathjs'; // Assuming mathjs is installed
 export default function Buttons() {
     const buttons = [1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, '*',0,'00','.', '/', 'ac', 'c'];
     const [btnNumber, setBtn] = useState('');
+    const [result, setResult] = useState('');
 
     const handleButtonClick = (value) => {
         if (value === 'c') {
             setBtn(''); // Clear all
+            setResult('')
         } else if (value === 'ac') {
             setBtn(btnNumber.slice(0, -1)); // Remove last character
-            
+            // setResult(result.slice(0,-1))
         } else {
             setBtn(btnNumber + value); // Append number or operator
         }
@@ -46,12 +48,14 @@ export default function Buttons() {
             </div>
         </div>
         <div className="input-container">
-            <p>{btnNumber}</p>
             <input type="text" 
                 value={btnNumber} 
                 onChange={e => setBtn(e.target.value)} 
                 // style={{ width: "100%", position: "absolute", top: "0",height:'50px' }} 
                 />
+                
+            <p>{result}</p>
+
         </div>
             
             <ul style={{ position: 'relative' }}>
@@ -61,8 +65,8 @@ export default function Buttons() {
                     <button onClick={() => {
                         try {
                             
-                            setBtn(evaluate(btnNumber));
-                            console.log(btnNumber.type)
+                            setResult(evaluate(btnNumber));
+                            // setRe/sult(btnNumber)
                         } catch (error) {
                             console.error("Invalid expression");
                         }
--- src/index.js
+++ src/index.js
@@ -5,20 +5,11 @@ import './all.css';
 import './all.min.css';
 import App from './components/App/App';
 import './all.min.css';
-
 import reportWebVitals from './reportWebVitals';
-// import Header from "./components/Header/Header";
-// import SideBar from "./components/SideBar/SideBar";
-// import Footer from './components/Footer/Footer'
-// import Chat from "./components/Chat/Chat";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <Header />
-    <SideBar />
-    <Chat />
-    <Footer /> */}
     <App />
   </React.StrictMode>
 );
